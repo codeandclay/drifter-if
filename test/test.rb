ad5c15131ae21fe1a3fad725a78eeaaaa25dcb4a
Oliver Denman <oliver@codeandclay.com>
2024-02-23 07:53:56 +0000

    WIP Remove superfluous flag? from Action
    An action will return a status. Whether or not the action has executed should
    be determined from the status.
    
    I'm unsure how to test event now… Perhaps an additional class can be used to
    determine whether or not an event has completed?

--- test/test.rb
+++ test/test.rb
@@ -13,28 +13,6 @@ describe Action do
   it "Should run successfully" do
     assert_equal :success, action.run
   end
-
-  it "Should not be executed when not run" do
-    refute action.executed?
-  end
-
-  it "Should be executed when run" do
-    action.run
-    assert action.executed?
-  end
-
-  it "Should not be executed once run and reset" do
-    action.run
-    action.reset
-
-    refute action.executed?
-  end
-
-  it "Should not be executed when reset before running" do
-    action.reset
-
-    refute action.executed?
-  end
 end
 
 describe Event do
@@ -42,65 +20,75 @@ describe Event do
   let(:event) { Event.new(actions: [action] )}
 
   it "Should run an event" do
+    skip
+
     event.fire
 
-    assert action.executed?
+    # assert action.executed?
   end
 
   it "Should run an event before firing" do
-    before_action = Action.new
-    before_event = Event.new(actions: [before_action] )
-    event = Event.new(actions: [action], options: { before_events: [ before_event ] } )
+    skip
 
-    refute before_action.executed?
-    event.fire
+    # before_action = Action.new
+    # before_event = Event.new(actions: [before_action] )
+    # event = Event.new(actions: [action], options: { before_events: [ before_event ] } )
 
-    assert action.executed?
-    assert before_action.executed?
+    # refute before_action.executed?
+    # event.fire
+
+    # assert action.executed?
+    # assert before_action.executed?
   end
 
   it "Should run an event after firing" do
-    after_action = Action.new
-    after_event = Event.new(actions: [after_action] )
-    event = Event.new(actions: [action], options: { after_events: [ after_event ] } )
+    skip
 
-    refute after_action.executed?
-    event.fire
+    # after_action = Action.new
+    # after_event = Event.new(actions: [after_action] )
+    # event = Event.new(actions: [action], options: { after_events: [ after_event ] } )
 
-    assert action.executed?
-    assert after_action.executed?
+    # refute after_action.executed?
+    # event.fire
+
+    # assert action.executed?
+    # assert after_action.executed?
   end
 
   it "Should run an event before and after firing" do
-    before_action = Action.new
-    before_event = Event.new(actions: [before_action] )
+    skip
 
-    after_action = Action.new
-    after_event = Event.new(actions: [after_action] )
+    # before_action = Action.new
+    # before_event = Event.new(actions: [before_action] )
+
+    # after_action = Action.new
+    # after_event = Event.new(actions: [after_action] )
     
-    event = Event.new(actions: [action], options: {
-      before_events: [ before_event ],
-      after_events: [ after_event ]
-    } )
+    # event = Event.new(actions: [action], options: {
+    #   before_events: [ before_event ],
+    #   after_events: [ after_event ]
+    # } )
 
-    refute before_action.executed?
-    refute action.executed?
-    refute after_action.executed?
+    # refute before_action.executed?
+    # refute action.executed?
+    # refute after_action.executed?
 
-    event.fire
+    # event.fire
 
-    assert before_action.executed?
-    assert action.executed?
-    assert after_action.executed?
+    # assert before_action.executed?
+    # assert action.executed?
+    # assert after_action.executed?
   end
 
   it "Should run multiple actions" do
+    skip
+
     total_actions = 5
 
     actions = Array.new(total_actions) { Action.new }
     event = Event.new(actions: actions).tap { |obj| obj.fire }
 
-    assert_equal total_actions, event.actions.count { |action| action.executed? }
+    # assert_equal total_actions, event.actions.count { |action| action.executed? }
   end
 
   it "Should register new action after creation" do
