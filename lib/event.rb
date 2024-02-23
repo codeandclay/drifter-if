f47e7690acc8b531559a9c2c19875fe80ee38b0a
Oliver Denman <oliver@codeandclay.com>
2024-02-23 11:42:22 +0000

    Make event immutable
    If I need to fire more actions or events, I can create a new class. Keeps
    Event much simpler.

--- lib/event.rb
+++ lib/event.rb
@@ -7,27 +7,6 @@ class Event
     @after_events = options[:after_events] ||= []
   end
 
-  def method_missing(method_called, arg)
-    arrays = {
-      register_before: before_events,
-      register_after: after_events,
-      remove_before: before_events,
-      remove_after: after_events
-    }
-
-    raise ArgumentError, "Method not found" unless arrays.keys.include? method_called
-
-    send method_called.to_s.split('_')[0].to_sym, arrays[method_called], arg
-  end
-
-  def register(collection = actions, item)
-    collection << item unless collection.include? item
-  end
-
-  def remove(collection = actions, item_to_remove)
-    collection.reject! { |item| item == item_to_remove }
-  end
-
   def fire
     fire_before_events
 
--- test/test.rb
+++ test/test.rb
@@ -29,55 +29,14 @@ describe Event do
 
   it "Should run an event before firing" do
     skip
-
-    # before_action = Action.new
-    # before_event = Event.new(actions: [before_action] )
-    # event = Event.new(actions: [action], options: { before_events: [ before_event ] } )
-
-    # refute before_action.executed?
-    # event.fire
-
-    # assert action.executed?
-    # assert before_action.executed?
   end
 
   it "Should run an event after firing" do
     skip
-
-    # after_action = Action.new
-    # after_event = Event.new(actions: [after_action] )
-    # event = Event.new(actions: [action], options: { after_events: [ after_event ] } )
-
-    # refute after_action.executed?
-    # event.fire
-
-    # assert action.executed?
-    # assert after_action.executed?
   end
 
   it "Should run an event before and after firing" do
     skip
-
-    # before_action = Action.new
-    # before_event = Event.new(actions: [before_action] )
-
-    # after_action = Action.new
-    # after_event = Event.new(actions: [after_action] )
-    
-    # event = Event.new(actions: [action], options: {
-    #   before_events: [ before_event ],
-    #   after_events: [ after_event ]
-    # } )
-
-    # refute before_action.executed?
-    # refute action.executed?
-    # refute after_action.executed?
-
-    # event.fire
-
-    # assert before_action.executed?
-    # assert action.executed?
-    # assert after_action.executed?
   end
 
   it "Should run multiple actions" do
@@ -89,109 +48,5 @@ describe Event do
     event = Event.new(actions: actions).tap { |obj| obj.fire }
 
     # assert_equal total_actions, event.actions.count { |action| action.executed? }
-  end
-
-  it "Should register new action after creation" do
-    original_action_count = event.actions.count
-
-    event.register(Action.new)
-
-    assert_equal original_action_count + 1, event.actions.count
-  end
-
-  it "Should remove actions from actions" do
-    original_action_count = event.actions.count
-
-    event.remove(action)
-
-    assert_equal original_action_count - 1, event.actions.count
-  end
-
-  it "Actions should be unique" do
-    original_action_count = event.actions.count
-
-    event.register(action)
-
-    assert_equal original_action_count, event.actions.count
-  end
-
-  it "Should register new before events" do
-    before_action = Action.new
-    before_event = Event.new(actions: [before_action] )
-    original_before_event_count = event.before_events.count
-
-    event.register_before(before_event)
-
-    assert_equal original_before_event_count + 1, event.before_events.count
-  end
-
-  it "Should register new after events" do
-    after_action = Action.new
-    after_event = Event.new(actions: [after_action] )
-    original_after_event_count = event.after_events.count
-
-    event.register_after(after_event)
-
-    assert_equal original_after_event_count + 1, event.after_events.count
-  end
-
-  it "Remove before and after events" do
-    original_before_event_count = event.before_events.count
-    original_after_event_count = event.after_events.count
-
-    before_action = Action.new
-    before_event = Event.new(actions: [before_action] )
-
-    after_action = Action.new
-    after_event = Event.new(actions: [after_action] )
-
-    event.register_before(before_event)
-    event.register_after(after_event)
-
-    event.remove_before(before_event)
-    event.remove_after(after_event)
-
-    assert_equal original_before_event_count, event.before_events.count
-    assert_equal original_after_event_count, event.after_events.count
-  end
-
-  it "Events should be unique" do
-    original_before_event_count = event.before_events.count
-    original_after_event_count = event.after_events.count
-
-    before_action = Action.new
-    before_event = Event.new(actions: [before_action] )
-
-    after_action = Action.new
-    after_event = Event.new(actions: [after_action] )
-
-    10.times do
-      event.register_before(before_event)
-    end
-
-    10.times do
-      event.register_after(after_event)
-    end
-
-    event.remove_before(before_event)
-    event.remove_after(after_event)
-
-    assert_equal original_before_event_count, event.before_events.count
-    assert_equal original_after_event_count, event.after_events.count
-  end
-
-  it "Will always return an array even when all events removed" do
-    before_action = Action.new
-    before_event = Event.new(actions: [before_action] )
-
-    10.times do
-      event.register_before(before_event)
-    end
-
-    20.times do
-      event.remove_before(before_event)
-    end
-
-    assert_equal [], event.before_events
   end
 end
